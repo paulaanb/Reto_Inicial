e04aacf61dce16f85576a7f086f3afec4593ef27
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-09-09 16:06:21 +0200

    creamos los movimientos del tablero

---
+++ codigo.py
@@ -0,0 +1,13 @@
+#Creamos el tablero con los movimientos que puede realizar cada numero dentro del tablero
+movimiento_tablero = {
+    0: [6, 4],
+    1: [6, 8],
+    2: [7, 9],
+    3: [4, 8],
+    4: [3, 9, 0],
+    5: [],
+    6: [1, 7, 0],
+    7: [2, 6],
+    8: [1, 3],
+    9: [2, 4]
+}
